a13b44b505381ebfd15c903f0d361790cfc6acec
ttak0422 <bgdaewalkman@gmail.com>
2019-08-12 11:09:24 +0900

    add comment

--- src/HelloWorld01/Main.fs
+++ src/HelloWorld01/Main.fs
@@ -1,8 +1,23 @@
-﻿module HelloWorld01
+﻿// 一行のコメント
 
+(*
+    複数行の
+    コメント
+    (* コメントのネスト *)
+*)
+
+// exportするモジュールの宣言
+module HelloWorld01
+
+// importするモジュールの宣言
 open Elmish
 open Elmish.React
 open Fable.React
+
+(*
+    Elmとは違いElmishではTEAのmodel, update, viewに従う必要がある
+    ここではダミーのmodel, update, モデルに依存しないviewを宣言している
+*)
 
 module Types =
     type Model = Dummy
@@ -15,7 +30,9 @@ module State =
     let update msg model = model
 
 module View =
-    let root model dispatch =
+    // 型宣言を書く必要はないものの書いておく方がいい場面もある
+    // 型宣言を書くにはこう
+    let root (model: Types.Model) dispatch : ReactElement =
         str "Hello, World"
 
 Program.mkSimple State.init State.update View.root
