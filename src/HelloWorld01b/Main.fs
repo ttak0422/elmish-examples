57fc0fc9c421980ebfa539e5e73385aea9c6f7c3
ttak0422 <bgdaewalkman@gmail.com>
2019-08-14 20:50:08 +0900

    update 01b

--- src/HelloWorld01b/Main.fs
+++ src/HelloWorld01b/Main.fs
@@ -1,17 +1,15 @@
 ﻿module HelloWorld01b.Main
 
 (*
-    単一のファイルにModel, Update, Viewを書くことはまずない。
-    ファイルを分割するならこんな感じ。
-    F#の前方参照を基本とする制約はプロジェクトが大きくなるにつれて
-    その効果を発揮する。
+    Examples of splitting a file.
+    Constraints based on F # forward references become 
+    more effective as the project grows.
 
     <Compile Include="Types.fs" />
     <Compile Include="State.fs" />
     <Compile Include="View.fs" />
     <Compile Include="Main.fs" />
 
-    *.fsprojのファイルの順番がそのまま参照の順番になる。
 *)
 
 open Types
