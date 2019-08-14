b18a83529bca1493a3fcfec64d3170a73580a09e
ttak0422 <bgdaewalkman@gmail.com>
2019-08-14 21:22:04 +0900

    update 03

--- src/HelloWorld03/Main.fs
+++ src/HelloWorld03/Main.fs
@@ -17,7 +17,12 @@ module State =
 module View =
     let root model dispatch : ReactElement =
         (*
-            １つ目のリストには属性を、２つ目のリストにはHTML要素のリストが入る。
+            We've made it so the view value isn't  just a text node any more.
+            It's now a div element with a text node as a child.
+            The Fable.React module has all the ReactElement elements you need.
+            div takes 2 arguments which are both lists.
+            The first list is a list of attributes, the second list is a list of child ReactElement elements.
+            We can nest elements the same way we normally do with HTML.
         *)
         div [] [ str "Hello, World" ]
 
